6d0db5e587220c7febab2f180972a44b9db0b602
PrabhudesaiNandan <nandan.prabhudesai@kilowott.com>
2025-05-27 18:04:43 +0530

    resolved error

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import "./ImageGenerator.css";
 import Home from "./components/Home";
-import Temp from "./components/Temp";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      {/* <Temp /> */}
     </div>
   );
 }
